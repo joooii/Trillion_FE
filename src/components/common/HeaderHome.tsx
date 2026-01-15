5818ba5ebb52e323ceac686c631bb879a512fdcc
joooii <3098k@naver.com>
2026-01-16 01:13:35 +0900

    [TRL-35] [fix] Home Header 서버컴포넌트로 변경

--- src/components/common/HeaderHome.tsx
+++ src/components/common/HeaderHome.tsx
@@ -1,11 +1,11 @@
-'use client';
+"server-only";
 
-import Image from 'next/image';
-import LogoMini from '@/assets/images/logo_mini.svg';
+import Image from "next/image";
+import LogoMini from "@/assets/images/logo_mini.svg";
 
 export default function HeaderHome() {
   return (
-    <header className="fixed top-0 left-1/2 -translate-x-1/2 w-[393px] pt-[65px] bg-primary-500 z-40">
+    <header className="fixed top-0 left-1/2 -translate-x-1/2 w-[393px] pt-[65px] bg-primary-500">
       <div className="h-[90px] flex flex-col px-4 pl-6 text-white">
         <div className="flex items-center gap-2">
           <div className="w-8 h-8 rounded-xl bg-white/20 flex items-center justify-center">
@@ -17,10 +17,11 @@ export default function HeaderHome() {
               className="object-contain"
             />
           </div>
+
           <p className="text-[25px] font-extrabold leading-none">SO:U+</p>
         </div>
 
-        <p className="mt-2">안녕하세요, 김소유님</p>
+        <p className="mt-2 font-medium">안녕하세요, 김소유님</p>
       </div>
     </header>
   );
