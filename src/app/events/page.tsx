d2087f1a64129643bece97e3f6358483bda6fcca
joooii <3098k@naver.com>
2026-01-16 01:19:01 +0900

    [TRL-35] [feat] 혜택 page header 추가

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/common/Header";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,14 +8,12 @@ export const metadata: Metadata = {
 export default function BenefitsPage() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-suite-semibold text-text-darkgray mb-4">
-        혜택
-      </h1>
+      <Header back text="혜택" />
       <div className="space-y-4">
         <div className="p-4 bg-white rounded-lg shadow-sm border border-gray-100">
-          <p className="text-text-lightgray">혜택 페이지입니다.</p>
+          <p className="text-text-lightgray">설정 페이지입니다.</p>
         </div>
       </div>
     </div>
   );
-} 
+}
