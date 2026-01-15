445d5a87fe9b6f0db7d2bc2cfd23298aecea8a27
joooii <3098k@naver.com>
2026-01-16 01:19:14 +0900

    [TRL-35] [feat] 홈 page 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
-import HeaderHome from '@/components/common/HeaderHome';
-import type { Metadata } from 'next';
+"server-only";
+
+import type { Metadata } from "next";
+import HeaderHome from "@/components/common/HeaderHome";
+import ContentSection from "@/components/home/ContentSection";
+import HelperSection from "@/components/home/HelperSection";
+import SummaryStartCard from "@/components/home/SummaryStartCard";
 
 export const metadata: Metadata = {
-  title: '홈',
+  title: "Home",
 };
 
 export default function HomePage() {
   return (
-    <div className="p-6">
+    <div className="pt-[139px] w-[393px] mx-auto">
       <HeaderHome />
-      <div className="space-y-4">
-        <div className="p-4 bg-white rounded-lg shadow-sm border border-gray-100">
-          <p className="text-text-lightgray">홈 페이지입니다.</p>
-        </div>
-      </div>
+      <SummaryStartCard />
+      <ContentSection />
+      <HelperSection />
     </div>
   );
 }
