91f507b4664ca481b6a5fb3330f2d11811701eac
hyunw-kang <vpffp368@naver.com>
2026-01-19 23:21:10 +0900

    [TRL-56] [fix] Setting UI 스크롤 수정

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -6,10 +6,11 @@ export default function SettingsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="pt-[155px]">
+    <div className="flex flex-col items-center">
       <HeaderHome
         title="설정"
         description="서비스 이용을 위한 설정을 관리하세요"
+        isScrollable={true}
       />
       {children}
     </div>
