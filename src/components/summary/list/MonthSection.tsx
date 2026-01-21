be36e5a9405cb4204c684a4088ccb3bf985734d6
joooii <3098k@naver.com>
2026-01-21 19:26:31 +0900

    [TRL-64] [fix] month와 카드 사이 30px로 수정

--- src/components/summary/list/MonthSection.tsx
+++ src/components/summary/list/MonthSection.tsx
@@ -4,7 +4,7 @@ interface MonthSectionProps {
 
 export default function MonthSection({ month }: MonthSectionProps) {
   return (
-    <div className="flex items-center gap-3 mb-2 mt-2">
+    <div className="flex items-center gap-3 mb-2 mt-[18px]">
       <p className="text-[20px] font-bold text-secondary-800">{month}월</p>
       <div className="flex-1 h-px bg-secondary-800/40" />
     </div>
