3d61975008ecf6990e10fe577ca40e0d6b284144
joooii <3098k@naver.com>
2026-01-22 11:02:28 +0900

    [TRL-69] [feat] 상담 내역 list UI 및 mock data 구현

--- src/app/(with-tab)/chat/page.tsx
+++ src/app/(with-tab)/chat/page.tsx
@@ -1,18 +1,29 @@
+import { ChevronRight } from "lucide-react";
 import type { Metadata } from "next";
-import ChatInputContainer from "@/components/chat/ChatInputContainer";
-import ChatActionButtons from "@/components/chat/ChatActionButtons";
+import Link from "next/link";
+
+import mockData from "@/mock/mockChatData.json";
 
 export const metadata: Metadata = {
-  title: "채팅",
+  title: "상담 내역",
 };
 
 export default function ChatPage() {
   return (
-    <div className="pt-[96px] flex flex-col items-center">
-      {/* 네모 박스 */}
-      <ChatInputContainer />
-      {/* 취소, 요약 시작 버튼 */}
-      <ChatActionButtons />
+    <div className="flex flex-col items-center gap-y-3">
+      {mockData.map((chat) => (
+        <Link key={chat.id} href={`/chat/${chat.id}`}>
+          <div className="flex items-center justify-between w-[335px] min-h-[70px] bg-white rounded-[10px] shadow-card px-[16px] py-[20px]">
+            <div>
+              <p className="text-text-darkgray text-[16px]">{chat.title}</p>
+              <p className="text-[10px] text-text-lightgray">
+                {chat.createdAt}
+              </p>
+            </div>
+            <ChevronRight className="w-[24px] h-[24px] text-text-lightgray flex items-center" />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
