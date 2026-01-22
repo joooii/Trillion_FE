9022746817bc9960920dc092c938f944e5880173
hyunw-kang <vpffp368@naver.com>
2026-01-22 11:27:08 +0900

    [TRL-62] [feat] EmptySummaryCard 컴포넌트 구현

--- src/components/home/SummaryList.tsx
+++ src/components/home/SummaryList.tsx
@@ -1,22 +1,25 @@
 import { SummaryHome } from "@/types/summaryList";
-import { Calendar, ChevronRight } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 import Summary from "@/components/home/Summary";
+import EmptyState from "@/components/home/EmptySummary";
 
-const mockData: SummaryHome[] = [
-  {
-    id: 1,
-    title: "온라인 결제 시스템 도입",
-    date: "2025.07.30",
-    content: "QR 코드 기반 결제 시스템 도입 희망",
-  },
-  {
-    id: 2,
-    title: "온라인 결제 시스템 도입",
-    date: "2025.07.30",
-    content: "QR 코드 기반 결제 시스템 도입",
-  },
-];
+// const mockData: SummaryHome[] = [
+//   {
+//     id: 1,
+//     title: "온라인 결제 시스템 도입",
+//     date: "2025.07.30",
+//     content: "QR 코드 기반 결제 시스템 도입 희망",
+//   },
+//   {
+//     id: 2,
+//     title: "온라인 결제 시스템 도입",
+//     date: "2025.07.30",
+//     content: "QR 코드 기반 결제 시스템 도입",
+//   },
+// ];
+
+const mockData: SummaryHome[] = [];
 
 export default function SummaryList() { 
   return (
@@ -35,11 +38,19 @@ export default function SummaryList() {
         )}
       </div>
 
-      <div className="flex flex-col gap-[10px]">
-        {mockData.map((item) => (
-          <SummaryCard key={item.id} {...item} />
-        ))}
-      </div>
+      {mockData.length > 0 ? (
+        <div className="flex flex-col gap-[10px]">
+          {mockData.map((item) => (
+            <SummaryCard key={item.id} {...item} />
+          ))}
+        </div>
+      ) : (
+        <EmptyState 
+          text="요약된 상담이 없습니다"
+          buttonText="요약 시작하기"
+          buttonLink="/chat"
+        />
+      )}
     </div>
   );
 }
